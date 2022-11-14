1798bbc8c20e15e499dab4a7c1015b1e1859ab1c
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-13 21:46:28 -0700

    Added two Events.  Still working the third

--- apps.js
+++ apps.js
@@ -1,4 +1,33 @@
 
+// Setting the background color
 
-let body = document.body
-setTimeout
+let body = document.body;
+body.style.backgroundColor = `bisque`
+
+// This event will turn the header red
+
+function makeRed(details) {
+    details.target.style.color = `red`;
+}
+let header = document.getElementById(`header`);
+header.addEventListener(`click`, makeRed);
+
+// This will change the image on mouse over
+
+function changeImg(details) {
+    details.target.src = `https://www.comingsoon.net/assets/uploads/2018/08/conair-618x360.jpg`;
+}
+
+let image = document.getElementById(`cage`);
+image.addEventListener(`mouseover`,changeImg);
+
+// This should change font family on space bar
+
+function fontfamily(details) {
+    details.body.style.fontfamily = 'cursive';
+}
+
+let font = document.body;
+if (`keycode === 32`) {
+    font.addEventListener('keydown', fontfamily);
+}
