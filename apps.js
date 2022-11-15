275b9d5ca44ebd3ff33641db833f1f19abdfedc0
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-14 20:17:38 -0700

    Attempted to get last event to work without success

--- apps.js
+++ apps.js
@@ -17,17 +17,15 @@ header.addEventListener(`click`, makeRed);
 function changeImg(details) {
     details.target.src = `https://www.comingsoon.net/assets/uploads/2018/08/conair-618x360.jpg`;
 }
-
 let image = document.getElementById(`cage`);
 image.addEventListener(`mouseover`,changeImg);
 
-// This should change font family on space bar
+// This should change background color on space bar
 
-function fontfamily(details) {
-    details.body.style.fontfamily = 'cursive';
+function changeColor(details) {
+    details.target.style.color = `aqua`;
 }
 
-let font = document.body;
-if (`keycode === 32`) {
-    font.addEventListener('keydown', fontfamily);
-}
+let Color = document.getElementById(`secondHeader`); 
+    Color.addEventListener(`keydown`,changeColor);   
+ 
